4099edb34e74e74dbfbf31096863a607047da6c7
yabramuvdi <yabran.muvdi@gmail.com>
2024-07-09 09:40:30 +0200

    add claude3.5

--- code/fed_example/llm_anthropic.py
+++ code/fed_example/llm_anthropic.py
@@ -1,3 +1,92 @@
 """
 Uses Anthorpic's API to label text data.
 """
+#%%
+
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+import yaml
+from anthropic import Anthropic
+
+# main paths
+data_path = "../../data/"
+model_path = "../../models/"
+output_path = "../../output/"
+keys_path = "../../../keys/"    # of course not in the repo ;)
+
+# read main parameters from file
+with open("params.yaml") as stream:
+    params = yaml.safe_load(stream)
+
+#%%
+
+with open(keys_path + 'anthropic_api.txt', 'r') as file:
+    api_key = file.read()
+
+client = Anthropic(api_key=api_key)
+
+#%%
+
+# read train and test data 
+df_finetune = pd.read_csv(data_path + "fed_train.csv", 
+                          dtype={'sentiment': 'category'})
+df_test = pd.read_csv(data_path + "fed_test.csv",
+                      dtype={'sentiment': 'category'})
+
+num_categories = len(df_finetune["sentiment"].cat.categories)
+
+
+# %%
+
+def generation_prompt(text):
+    prompt = f"""
+    Your task is to classify the text into one of the three categories ("dovish", "neutral", "hawkish").
+    The text is taken at random from multiple FOMC announcements.
+    Provide your output in JSON format with a key "category" and the selected category.
+
+    Text:
+    {text}
+    """
+    return prompt
+
+def classify_response(result):
+    if "hawkish" in result:
+        return "hawkish"
+    elif "dovish" in result:
+        return "dovish"
+    elif "neutral" in result:
+        return "neutral"
+    else:
+        return np.nan
+
+#%%
+
+# process all examples
+all_responses = []
+for text in tqdm(df_test["text"].sample(2)):
+    my_prompt = generation_prompt(text)
+    response = client.messages.create(
+        model=params["llm_anthropic_model"],
+        max_tokens=100,
+        temperature=params["llm_temperature"],
+        system="You are a research assistant working for the Fed. You have a degree in Economics.",
+        messages=[
+            {"role": "user",
+             "content": my_prompt,
+            },
+            
+            ],
+        )
+    result = response.content
+    all_responses.append(classify_response(result))
+
+#%%
+    
+# append data to existing dataframe
+df_test["prediction"] = all_responses
+df_test = df_test[["ID", "prediction"]] 
+df_test.to_csv(output_path + f"fed_tagged_{params['llm_anthropic_model']}.csv", index=False)
+
+
+#%%
--- code/policy_uncertainty_example/data_clean.py
+++ code/policy_uncertainty_example/data_clean.py
@@ -7,9 +7,12 @@ import pandas as pd
 import numpy as np
 import yaml
 import os
+import shutil
 
 # main paths
 data_path = "../../data/"
+articles_path = "../../../EPU/All Audit Hard Copies/All Audit Hard Copies/Modern/"
+output_path = "../../labeled_articles/"
 
 # read main parameters from file
 with open("params.yaml") as stream:
@@ -53,4 +56,24 @@ df_master_modern.drop_duplicates(inplace=True)
 
 df_complete = pd.merge(df_master, df_master_modern, on=["article_number", "year", "month"])
 df_complete.to_csv(data_path + "epu_modern_labels.csv", index=False)
+# %%
+
+#===============
+# Save only articles with labels
+#===============
+
+all_success = []
+for article in df_complete["unique_id_current"].values:
+    try:
+        print(article)
+        source_file = os.path.join(articles_path, article + ".html")
+        destination_file = os.path.join(output_path, article + ".html")
+        # Copy the file
+        shutil.copy2(source_file, destination_file)
+        print(f'Copied {source_file} to {destination_file}')
+        all_success.append(True)
+    except Exception as e:
+        print(f'An error occurred: {e} with file {article}')
+        all_success.append(False)
+        
 # %%
